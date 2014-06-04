73aef52ced27ceba855307446de5c6f2cc5f1f42
David Matson <githubdavid@gmail.com>
2014-06-04 16:56:34 -0700

    Update descriptions and put FLN plugin in caution folder

--- VanillaThemeNitrogen/plugins/QnA/ReadMe.md
+++ VanillaThemeNitrogen/plugins/QnA/ReadMe.md
@@ -1,8 +1,6 @@
 ###The Q&A Big Button Remix plugin
 
-David Matson (remixer only adding in a very small number of code changes suggested by peregrine), Todd Burry (original author)
-
-David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button. 
+Code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button have made to this plugin. 
 
 In addition to these code changes, add the two additional lines of code below (also copied from the above post) to your config.php file to make the Big Button appear: 
 
@@ -14,5 +12,4 @@ This remix is provided as a convenience saving you the time altering the origina
 
 To upgrade this plugin remix you will need to download the latest original (unremixed) version of the plugin http://vanillaforums.org/addon/734/q-a, and modify it with code changes found in the above forum post yourself, or hire someone to do it for you. You can check at the Help Give Thanks SourceForge folder to see if there is an upgrade available, but no promises: http://sourceforge.net/projects/helpgivethanks/files/HGTtemplate/.
 
-
-
+David Matson is to blame for adding in a very small number of code changes authored by VanillaForum.org contributor peregrine. Todd Burry is the original author of this wonderfully useful plugin.
--- VanillaThemeNitrogen/plugins/QnA/class.qna.plugin.php
+++ VanillaThemeNitrogen/plugins/QnA/class.qna.plugin.php
@@ -9,15 +9,16 @@ $PluginInfo['QnA'] = array(
    'Name' => 'Q&A Big Button Remix',
    'Description' => "Users may designate a discussion as a Question and then officially accept one or more of the comments as the answer.  
 
-NOTE: David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button. In addition to these code changes, two additional lines of code also copied from the above post must be added to the config.php file to add the big button as specified in this remix's readme.md file.
+NOTE: Code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button have made to this plugin. In addition to these code changes, two additional lines of code also available from the above post must be added to the config.php file to show the big button.  These additional lines of code are also specified in this remix's ReadMe file.
 
-This remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
+This plugin remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
 
 To upgrade this plugin remix you will need to download the latest original (unremixed) version of the plugin http://vanillaforums.org/addon/734/q-a, and modify it with code changes found in the above forum post yourself, or hire someone to do it for you. You can check at the Help Give Thanks SourceForge folder to see if there is an upgrade available, but no promises: http://sourceforge.net/projects/helpgivethanks/files/HGTtemplate/",
    'Version' => '1.2.1 Big Button Remix',
    'RequiredApplications' => array('Vanilla' => '2.0.18'),
    'MobileFriendly' => TRUE,
-   'Author' => 'David Matson (remixer only adding in a very small number of code changes suggested by peregrine), Todd Burry (original author)'
+   'Author' => 'David Matson (remixer only adding in a very small number of code changes authored by VanillaForum.org contributor peregrine), Todd Burry (original author)',
+   'AuthorUrl' => 'http://helpgivethanks.org/'
 );
 
 /**
