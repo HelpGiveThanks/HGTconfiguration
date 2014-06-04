9bd776b27f87643afd883a1cf000c2ec6231ee47
David Matson <githubdavid@gmail.com>
2014-06-03 17:58:23 -0700

    add info about code to add to config.php

--- VanillaThemeNitrogen/plugins/QnA/ReadMe.md
+++ VanillaThemeNitrogen/plugins/QnA/ReadMe.md
@@ -2,7 +2,13 @@
 
 David Matson (remixer only adding in a very small number of code changes suggested by peregrine), Todd Burry (original author)
 
-David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button.  
+David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button. 
+
+In addition to these code changes, add the two additional lines of code below (also copied from the above post) to your config.php file to make the Big Button appear: 
+
+$Configuration['Plugins']['QnA']['UseBigButtons'] = TRUE;
+
+$Configuration['Modules']['Vanilla']['Panel'] = array('MeModule', 'UserBoxModule', 'GuestModule', 'NewDiscussionModule', 'NewQuestionModule','DiscussionFilterModule', 'SignedInModule', 'Ads');
 
 This remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
 
--- VanillaThemeNitrogen/plugins/QnA/class.qna.plugin.php
+++ VanillaThemeNitrogen/plugins/QnA/class.qna.plugin.php
@@ -9,7 +9,9 @@ $PluginInfo['QnA'] = array(
    'Name' => 'Q&A Big Button Remix',
    'Description' => "Users may designate a discussion as a Question and then officially accept one or more of the comments as the answer.  
 
-NOTE: David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button.  This remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
+NOTE: David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button. In addition to these code changes, two additional lines of code also copied from the above post must be added to the config.php file to add the big button as specified in this remix's readme.md file.
+
+This remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
 
 To upgrade this plugin remix you will need to download the latest original (unremixed) version of the plugin http://vanillaforums.org/addon/734/q-a, and modify it with code changes found in the above forum post yourself, or hire someone to do it for you. You can check at the Help Give Thanks SourceForge folder to see if there is an upgrade available, but no promises: http://sourceforge.net/projects/helpgivethanks/files/HGTtemplate/",
    'Version' => '1.2.1 Big Button Remix',
