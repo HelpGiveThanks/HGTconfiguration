b4d2b6bb52f5c85583b415ad88077cff4d9715de
David Matson <githubdavid@gmail.com>
2014-06-05 01:12:02 -0700

    Revert FLN plugin

--- VanillaThemeNitrogen/plugins/CAUTION/FirstLastNames/views/registerbasic.php
+++ VanillaThemeNitrogen/plugins/CAUTION/FirstLastNames/views/registerbasic.php
@@ -1,44 +1,50 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
-<div class="FormTitleWrapper">
 <h1><?php echo T("Apply for Membership") ?></h1>
-<div class="FormWrapper">
-
-
-
-   <?php // Change terms url to your TGB wiki page's url.
-   $TermsOfServiceUrl = 'http://YOURSITE.COM/YOURTBGfolder/thebuggenie/wiki/TermsOfUse';
-   $TermsOfServiceText = sprintf(T('I agree to the <a id="TermsOfService" target="terms" href="%s">terms of use</a>'), Url($TermsOfServiceUrl));
-   
-   
-   
+<div class="Box">
+   <?php
+   $TermsOfServiceUrl = Gdn::Config('Garden.TermsOfService', '#');
+   $TermsOfServiceText = sprintf(T('I agree to the <a id="TermsOfService" class="Popup" target="terms" href="%s">terms of service</a>'), Url($TermsOfServiceUrl));
    // Make sure to force this form to post to the correct place in case the view is
    // rendered within another view (ie. /dashboard/entry/index/):
    echo $this->Form->Open(array('Action' => Url('/entry/register'), 'id' => 'Form_User_Register'));
    echo $this->Form->Errors();
    ?>
    <ul>
-      <?php if (!$this->Data('NoEmail')): ?>
       <li>
          <?php
             echo $this->Form->Label('Email', 'Email');
-            echo $this->Form->TextBox('Email', array('type' => 'email', 'Wrap' => TRUE));
+            echo $this->Form->TextBox('Email');
             echo '<span id="EmailUnavailable" class="Incorrect" style="display: none;">'.T('Email Unavailable').'</span>';
          ?>
       </li>
-      <?php endif; ?>
       <li>
          <?php
             echo $this->Form->Label('Username', 'Name');
-            echo $this->Form->TextBox('Name', array('Wrap' => TRUE, 'autocorrect' => 'off', 'autocapitalize' => 'off'));
+            echo $this->Form->TextBox('Name');
             echo '<span id="NameUnavailable" class="Incorrect" style="display: none;">'.T('Name Unavailable').'</span>';
          ?>
       </li>
-                  
-      
-      
-      	  <li class="TextBoxWrapper" >
-      	  
-         <?php // This is the only code from Q&A pluggin that I kept.  The entry forum code for Vanilla 2.1 looks better in my opinion so used it and just inserted this code from the Q&A pluggin rewrite to add in fields for first and last name.
+      <li>
+         <?php
+            echo $this->Form->Label('Password', 'Password');
+            echo $this->Form->Input('Password', 'password');
+         ?>
+      </li>
+      <li>
+         <?php
+            echo $this->Form->Label('Confirm Password', 'PasswordMatch');
+            echo $this->Form->Input('PasswordMatch', 'password');
+            echo '<span id="PasswordsDontMatch" class="Incorrect" style="display: none;">'.T("Passwords don't match").'</span>';
+         ?>
+      </li>
+      <li class="Gender">
+         <?php
+            echo $this->Form->Label('Gender', 'Gender');
+            echo $this->Form->RadioList('Gender', $this->GenderOptions, array('default' => 'm'))
+         ?>
+      </li>
+	  <li>
+         <?php
             if( C('Plugins.FirstLastNames.NickName') )
 				echo $this->Form->Label('Nickname', 'FirstName');
 			else
@@ -47,56 +53,22 @@
          ?>
       </li>
 	  <?php if( !C('Plugins.FirstLastNames.NickName') ) { ?>
-	  <li class="TextBoxWrapper">
+	  <li>
          <?php
             echo $this->Form->Label('Last Name', 'LastName');
             echo $this->Form->TextBox('LastName');
          ?>
       </li>
 	  <?php } ?>
-      
-      
-  
-      <?php $this->FireEvent('RegisterBeforePassword'); ?>
       <li>
          <?php
-            echo $this->Form->Label('Password', 'Password');
-            echo Wrap(sprintf(T('Your password must be at least %d characters long.'), C('Garden.Registration.MinPasswordLength')), 'div', array('class' => 'Gloss')); 
-            echo $this->Form->Input('Password', 'password', array('Wrap' => TRUE, 'Strength' => TRUE));
-         ?>
-      </li>
-      <li>
-         <?php
-            echo $this->Form->Label('Confirm Password', 'PasswordMatch');
-            echo $this->Form->Input('PasswordMatch', 'password', array('Wrap' => TRUE));
-            echo '<span id="PasswordsDontMatch" class="Incorrect" style="display: none;">'.T("Passwords don't match").'</span>';
-         ?>
-      </li>
-      <li class="Gender">
-         <?php
-            echo $this->Form->Label('Gender', 'Gender');
-            echo $this->Form->RadioList('Gender', $this->GenderOptions, array('default' => 'u'))
-         ?>
-      </li>
-      <?php if ($this->Form->GetValue('DiscoveryText') || GetValue('DiscoveryText', $this->Form->ValidationResults()) ): ?>
-      <li>
-         <?php
-            echo $this->Form->Label('Why do you want to join?', 'DiscoveryText');
-            echo $this->Form->TextBox('DiscoveryText', array('MultiLine' => TRUE, 'Wrap' => TRUE));
-         ?>
-      </li>
-      <?php endif; ?>
-      <?php $this->FireEvent('RegisterFormBeforeTerms'); ?>
-      <li>
-         <?php
-            echo $this->Form->CheckBox('TermsOfService', '@'.$TermsOfServiceText, array('value' => '1'));
+            echo $this->Form->CheckBox('TermsOfService', $TermsOfServiceText, array('value' => '1'));
             echo $this->Form->CheckBox('RememberMe', T('Remember me on this computer'), array('value' => '1'));
          ?>
       </li>
       <li class="Buttons">
-         <?php echo $this->Form->Button('Sign Up', array('class' => 'Button Primary')); ?>
+         <?php echo $this->Form->Button('Sign Up'); ?>
       </li>
    </ul>
    <?php echo $this->Form->Close(); ?>
-</div>
 </div>
