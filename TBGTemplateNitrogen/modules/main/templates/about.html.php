fa959d9018276d4c96818e074b6b0e849dc9395c
David Matson <githubdavid@gmail.com>
2014-06-05 00:29:19 -0700

    Update notice of changes to make them minor

--- TBGTemplateNitrogen/modules/main/templates/about.html.php
+++ TBGTemplateNitrogen/modules/main/templates/about.html.php
@@ -15,18 +15,9 @@
 		
 		
 		<br>
-		<b>NOTE: 33 files have been changed in The Bug Genie v3.2.7.1 release and 2 added to accomplish and explain a template change entitled nitrogen.</b>
-		<br>
-		To compare CURRENT differences between the nitrogen template remix of TBG files and the original TBG v3.2.7.1 files, please visit <a target="_blank" href="https://github.com/virtualdavid/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1"> https://github.com/virtualdavid/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1 </a> <i>(opens in a new window)</i><br>
-		<br>  
-		To compare differences between this version (version 1) of nitrogen and 3.2.7.1, view file "Description of Nitrogen Template v1 Modifications.html" available for download from SourceForge (link below).<br>
-		<br>
-		To see screenshots and read a detailed explanation about the remix go here <a target="_blank" href="http://forum.thebuggenie.com/viewtopic.php?f=9&t=1691&sid=628e6e5f3027e56f07651c066c001557"> http://forum.thebuggenie.com/viewtopic.php?f=9&t=1691&sid=628e6e5f3027e56f07651c066c001557 </a> <i>(opens in a new window)</i><br>
-		<br>
-		To download a copy of the template files go here <a target="_blank" href="https://sourceforge.net/projects/helpgivethanks/"> https://sourceforge.net/projects/helpgivethanks/ </a> <i>(opens in a new window)</i> The ReadME.txt file that comes with this template explains how to customize the template files for your Bug Genie, and then perform a simple drop-in replace to affect the template’s changes.<br>
+		<b>NOTE: The Nitrogen Template has been applied to this copy of The Bug Genie primarily affecting its look and feel. </b> Scroll down for more info.  
 		<br>	
-		<br>	
-		
+		<br>
 		
 		<h3 style="margin-top: 0; padding-top: 0;">Beautiful issue tracking and project management</h3>
 		<?php echo __('The Bug Genie is an issue tracking system with a strong focus on being friendly &ndash; both for regular users and power users'); ?>.<br>
@@ -45,4 +36,21 @@
 		<?php echo __('The Bug Genie uses icons from the %link_to_iconset%', array('%link_to_iconset%' => '<a href="http://www.oxygen-icons.org">Oxygen icon set</a>')); ?>.<br>
 		<?php echo __('These icons may be freely distributed under the %link_to_license%', array('%link_to_license%' => '<a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0 License</a>')); ?>.
 	</span>
+
+<div style="text-align: left; padding: 10px;">
+<br>
+		<b>The Nitrogen Template</b> by David Matson = 33 minor file changes and 2 additions to The Bug Genie (TGB) v3.2.7.1 primarily affecting its look and feel.  Thanks to Sharron Denice for code copied from TBG’s master branch to enable showing YouTube videos in text areas: https://github.com/thebuggenie/thebuggenie/commit/8005f8721b4fceaf725f7f36c61b077986251c7a.  
+		<br>
+		<br>
+		To compare CURRENT differences between the nitrogen template and original TBG v3.2.7.1 files, please visit <a target="_blank" href="https://github.com/HelpGiveThanks/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1"> https://github.com/HelpGiveThanks/thebuggenie/compare/thebuggenie:branch-32...nitrogen?expand=1 </a> <i>(opens in a new window)</i>
+		<br>
+		<br>
+		To see screenshots and read a detailed explanation about the template go here <a target="_blank" href="http://forum.thebuggenie.com/viewtopic.php?f=9&t=1691&sid=628e6e5f3027e56f07651c066c001557"> http://forum.thebuggenie.com/viewtopic.php?f=9&t=1691&sid=628e6e5f3027e56f07651c066c001557 </a> <i>(opens in a new window)</i>
+		<br>
+		<br>
+		To download a copy of the template files and learn how to customize and install them via drop-in replace go here <a target="_blank" href="https://sourceforge.net/projects/helpgivethanks/"> https://sourceforge.net/projects/helpgivethanks/ </a> <i>(opens in a new window)</i> 
+		<br>
+		<br>
+		The Nitrogen Template is also distributed under the MPL v1.1. as required in section 3.1. Application of License: “The Modifications which You create or to which You contribute are governed by the terms of this License…,” which can be <?php echo __('read at %link_to_MPL%', array('%link_to_MPL%' => '<a href="http://www.opensource.org/licenses/mozilla1.1.php">opensource.org</a>')); ?>.
+</div>
 </div>
