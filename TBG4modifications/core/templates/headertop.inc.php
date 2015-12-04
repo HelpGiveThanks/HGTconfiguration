db6ab411d5e4d4fc2fc9a3f95c2f18a785902dbd
David Matson <githubdavid@gmail.com>
2015-12-03 20:02:40 -0800

    Made room and added permanent nav buttons

--- TBG4modifications/core/templates/headertop.inc.php
+++ TBG4modifications/core/templates/headertop.inc.php
@@ -2,9 +2,17 @@
     <div id="logo_container">
         <?php \thebuggenie\core\framework\Event::createNew('core', 'header_before_logo')->trigger(); ?>
         <?php $link = (\thebuggenie\core\framework\Settings::getHeaderLink() == '') ? \thebuggenie\core\framework\Context::getWebroot() : \thebuggenie\core\framework\Settings::getHeaderLink(); ?>
-        <a class="logo" href="<?php print $link; ?>"><?php echo image_tag(\thebuggenie\core\framework\Settings::getHeaderIconUrl(), array('style' => 'max-height: 24px;'), \thebuggenie\core\framework\Settings::isUsingCustomHeaderIcon()); ?></a>
-        <div class="logo_name"><?php echo \thebuggenie\core\framework\Settings::getSiteHeaderName(); ?></div>
-    </div>
+
+
+
+        <?php //  111111111111111111111111111111111  ?>
+
+        <?php // Experiment with icon width and height to get best look. Keep the height amount at or below 30px, as anything greater will shift the breadcrumb text under it over the right. ?>
+        <a class="logo" href="<?php print $link; ?>"><img alt="icon" src="http://YOURWEBSITE.COM/YOURBUGGENIEFOLDER/public/header.png" style="width: 24px; height: 24px;"></img></a>
+            </div>
+
+
+
     <?php if (!\thebuggenie\core\framework\Settings::isMaintenanceModeEnabled()): ?>
         <div id="topmenu-container">
             <?php if (\thebuggenie\core\framework\Event::createNew('core', 'header_mainmenu_decider')->trigger()->getReturnValue() !== false): ?>
