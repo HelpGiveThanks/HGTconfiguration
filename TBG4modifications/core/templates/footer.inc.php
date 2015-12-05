5adfe4b58cf020d8b6e5503c91a97c72d0cb09bd
David Matson <githubdavid@gmail.com>
2015-12-04 20:55:32 -0800

    Customized footer

--- TBG4modifications/core/templates/footer.inc.php
+++ TBG4modifications/core/templates/footer.inc.php
@@ -1,10 +1,26 @@
 <footer>
-    <?php echo image_tag('footer_logo.png'); ?>
+
+
+  <?php // BEGIN MODIFY FOOTER ?>
+
+
+<?php // TRIED AND FAILED SO FAR TO ADD ONTO HOME URL echo __('%thebuggenie', array('%thebuggenie' => link_tag(make_url('home'), 'The Bug Genie')), true); ?>
+
+
+<a href="http://127.0.0.1/buggenie402/public/wiki/Privacy" target="_blank">Privacy</a> |
+<a href="http://127.0.0.1/buggenie402/public/wiki/TermsOfUse" target="_blank">Terms of Use</a> |
+<?php // echo __('Projects'); ?> Mess Bounder <?php echo __('%thebuggenie', array('%thebuggenie' => link_tag(make_url('about'), 'The Bug Genie')), true); ?> |
+Forum <a href="http://vanillaforums.org/" target="_blank">Vanilla</a> |
+Website Configuration <a href="http://helpgivethanks.org/apps/thebuggenie/wiki/CollaborativeWebsiteConfiguration" target="_blank">HGT</a> |
+Content Copyright (except where noted) <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
+
+
+    <?php /* echo image_tag('footer_logo.png'); ?>
     <?php echo __('%thebuggenie, <b>friendly</b> issue tracking since 2002', array('%thebuggenie' => link_tag(make_url('about'), 'The Bug Genie')), true); ?><br>
         <a href="http://opensource.org/licenses/MPL-2.0"><?php echo __('Read the license (MPL 2.0)'); ?></a>
     <?php if ($tbg_user->canAccessConfigurationPage()): ?>
         | <b><?php echo link_tag(make_url('configure'), __('Configure %thebuggenie_name', array('%thebuggenie_name' => \thebuggenie\core\framework\Settings::getSiteHeaderName()))); ?></b>
-    <?php endif; ?>
+    <?php endif; ?> <*/?>
     <?php if (\thebuggenie\core\framework\Context::isDebugMode() && \thebuggenie\core\framework\Logging::isEnabled()): ?>
         <script>
             function tbg_debug_show_menu_tab(tab, clicked) {
